449ea2d618f0ef3c605fb14d82951edfac75d9b8
Martin J. Lasek <martinlasek@hotmail.de>
2018-11-13 16:47:46 +0100

    adjusted router to new implementation of nutrition page

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,6 @@ import Landing from "./sites/landing";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Feedback from "./sites/feedback";
 import NutritionPage from "./components/renderings/nutritionpage";
-import NutritionPage2 from "./components/renderings/nutritionpage2";
-import NutritionPage3 from "./components/renderings/nutritionpage3";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -14,8 +12,6 @@ ReactDOM.render(
       <Route exact path="/" component={Landing} />
       <Route path="/menu" component={App} />
       <Route path="/nutrition" component={NutritionPage} />
-      <Route path="/nutrition2" component={NutritionPage2} />
-      <Route path="/nutrition3" component={NutritionPage3} />
       <Route path="/feedback" component={Feedback} />
     </Switch>
   </BrowserRouter>,
